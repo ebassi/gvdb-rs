ac0100c19fc01abfc3db310058408a41b4d3a641
Emmanuele Bassi <ebassi@gnome.org>
2017-08-29 23:09:18 +0100

    Add gvdb::Builder
    This is the Rust wrapper object to build a GVDB file; it can be used as
    a simple "configuration" type in order to quickly build a database from
    a known set of keys.
    
    Later, we'll add more API to ensure you can build a database
    programmatically, including nested tables.

---
+++ src/builder.rs
@@ -0,0 +1,102 @@
+//! Build and write a GVDB database
+//!
+//! ## Examples
+//!
+//! extern crate glib;
+//! extern crate gvdb;
+//!
+//! fn main() {
+//!     gvdb::Builder::new()
+//!                   .item("foo", glib::Variant::from("hello"))
+//!                   .item("bar", glib::Variant::from("world"))
+//!                   .item("baz", glib::Variant::from(42))
+//!                   .filename("entries.db")
+//!                   .write();
+//! }
+
+use std::path::{PathBuf, Path};
+use std::ptr;
+
+use glib::translate::*;
+use gvdb_sys;
+use Error;
+use Variant;
+
+#[derive(Clone, Debug)]
+pub struct Builder {
+    table: Vec<(String, Variant)>,
+    filename: Option<PathBuf>,
+    byteswap: bool,
+}
+
+impl Builder {
+    pub fn new() -> Builder {
+        Builder {
+            table: Vec::new(),
+            filename: None,
+            byteswap: false,
+        }
+    }
+
+    pub fn byteswap(&mut self, byteswap: bool) -> &mut Builder {
+        self.byteswap = byteswap;
+        self
+    }
+
+    pub fn item(&mut self, key: &str, value: Variant) -> &mut Builder {
+        self.table.push((key.to_string(), value.clone()));
+        self
+    }
+
+    pub fn write<P: AsRef<Path>>(&mut self, filename: P) -> Result<(), Error> {
+        unsafe {
+            let ht = gvdb_sys::gvdb_hash_table_new(ptr::null_mut(), ptr::null_mut());
+
+            for iter in &self.table {
+                let key = &iter.0;
+                let value = &iter.1;
+                let item = gvdb_sys::gvdb_hash_table_insert(ht, key.to_glib_none().0);
+
+                gvdb_sys::gvdb_item_set_value(item, value.to_glib_none().0);
+            }
+
+            let mut error = ptr::null_mut();
+            gvdb_sys::gvdb_table_write_contents(ht,
+                                                filename.as_ref().to_glib_none().0,
+                                                self.byteswap.to_glib(),
+                                                &mut error);
+            if error.is_null() {
+                Ok(())
+            }
+            else {
+                Err(from_glib_full(error))
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+#[cfg_attr(test, allow(dead_code))]
+mod tests {
+    use super::Builder;
+    use glib;
+
+    #[test]
+    fn build_empty() {
+        let _ = Builder::new().write("test-builder.db");
+    }
+
+    #[test]
+    fn build_byteswap() {
+        let _ = Builder::new().byteswap(true).write("test-builder.db");
+    }
+
+    #[test]
+    fn build_items() {
+        let _ =
+            Builder::new()
+                    .item("foo", glib::Variant::from("hello, world"))
+                    .item("bar", glib::Variant::from(42))
+                    .write("test-builder.db");
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+// Copyright 2017  Emmanuele Bassi
+// Released under the terms of the GNU Lesser General Public License, either
+// version 2.1 or, at your option, any later version.
+// See the LICENSE file for the licensing terms.
+
+//! # **gvdb** bindings for Rust
+//!
+//! GVDB is a simple database file format that stores a mapping between
+//! strings and values expressed as [GVariant](https://developer.gnome.org/glib/stable/glib-GVariant.html).
+//!
+//! The database is written once, and cannot be modified.
+
+#![cfg_attr(feature = "cargo-clippy", allow(doc_markdown))]
+
+extern crate libc;
+extern crate glib;
+extern crate gvdb_sys;
+
+use std::ffi::CStr;
+
+use glib::{
+    Bytes,
+    Error,
+    Variant,
+};
+
+pub mod builder;
+pub use builder::Builder;
